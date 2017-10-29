7d6fab0285ea6fdf42b81b9296d20d8ab6c7eb73
kaneko_takayuki <g.knk.9410@gmail.com>
2017-10-29 15:27:28 +0900

    SVMによる合議スクリプトを追加

--- exec/deeplearning/add_path.py
+++ exec/deeplearning/add_path.py
@@ -4,4 +4,4 @@ import os
 import sys
 
 base = os.path.dirname(os.path.abspath(__file__))
-sys.path.append(base + "/../")
+sys.path.append(base + "/../../")
---
+++ exec/summarize/exec_summarize_j_svm_w2v_majority.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+import argparse
+import add_path
+from analyzer import summarizer
+from analyzer.summarizer import j_svm_w2v
+from analyzer.summarizer import consult_function as cf
+
+
+def main(input_files, output_file):
+    """
+    日本語用FFNNで出力した結果を多数決によってまとめる
+    :param input_files: 入力ファイルリスト
+    :param output_file: 出力ファイル
+    :return: なし
+    """
+    # 合議関数を設定
+    summarizer.consult_func = cf.svm_consult_majority
+    # 実行
+    j_svm_w2v.summarize_svm_w2v(input_files, output_file)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='summarize svm-out to japanese binary classification with majority')
+    parser.add_argument("--input_files", "-i", nargs='*')
+    parser.add_argument("--output_file", "-o")
+    args = parser.parse_args()
+
+    main(input_files=args.input_files, output_file=args.output_file)
