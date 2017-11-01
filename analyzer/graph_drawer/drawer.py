1e789446e756389c10976f1a75e956b19e0e20a1
kaneko_takayuki <g.knk.9410@gmail.com>
2017-11-01 11:45:14 +0900

    drawerのメソッドに説明を追加

--- analyzer/graph_drawer/drawer.py
+++ analyzer/graph_drawer/drawer.py
@@ -5,6 +5,13 @@ import matplotlib.pyplot as plt
 
 
 def plot(left, input_file, label):
+    """
+    グラフ1つ分を描画する
+    :param left: 横軸の長さ
+    :param input_file: 描画する精度ファイル
+    :param label: どの色で描画するか
+    :return: なし
+    """
     height = []
     with open(input_file, 'r') as f:
         for line in f:
@@ -14,6 +21,13 @@ def plot(left, input_file, label):
 
 
 def draw_graph(left, input_files, labels):
+    """
+    折れ線グラフを描画する
+    :param left: 横軸の長さ
+    :param input_files: 描画する精度ファイルリスト
+    :param labels: どの色でグラフを表示するのか
+    :return: なし
+    """
     for input_file, label in zip(input_files, labels):
         plot(left, input_file, label)
     plt.legend()
