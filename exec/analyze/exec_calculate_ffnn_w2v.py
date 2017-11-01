a82c594770c0913fe134e036c37e825e928c50ea
kaneko_takayuki <g.knk.9410@gmail.com>
2017-11-01 10:04:27 +0900

    精度を計算するスクリプトをリファクタリング

---
+++ exec/analyze/exec_calculate_ffnn_w2v.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+import argparse
+from analyzer.calculator import calc_accuracy
+
+
+def main(experiment_dir, n_epoch):
+    """
+    日本語FFNNの出力について、精度を計算する
+    :param experiment_dir: 実験ディレクトリ
+    :param n_epoch: エポック数
+    :return: なし
+    """
+    # パラメータの表示
+    print("合議対象:")
+    print("\t- 日本語文\n\t- 2段階評価\n\t- FFNN")
+    print("対象ディレクトリ:")
+    print('\t- ' + experiment_dir)
+    print("-----------------------------------")
+
+    # 計算するファイルリストを用意
+    input_files = [experiment_dir + "epoch" + str(i) + ".txt" for i in range(1, n_epoch+1)]
+
+    # 精度を計算
+    calc_accuracy.calc_accuracy_epochs(input_files, experiment_dir + "accuracy_file.txt")
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='FFNNの合議による2値分類の精度を計算する')
+    parser.add_argument("--experiment_dir", "-d")
+    parser.add_argument("--n_epoch", "-e")
+    args = parser.parse_args()
+
+    main(experiment_dir=args.experiment_dir, n_epoch=args.n_epoch)
--- exec/analyze/exec_calculate_j_svm_w2v.py
+++ exec/analyze/exec_calculate_j_svm_w2v.py
@@ -1,56 +1,33 @@
 # -*- coding: utf-8 -*-
 
-import os
-import sys
+import argparse
 import add_path
 from analyzer.calculator import calc_accuracy
 
 
-def calculate_accuracy(experiment_dir):
+def main(experiment_dir):
     """
-    まとめた結果ファイルに対して、精度を計算してファイルに出力
-    :param experiment_dir: 実験結果ファイルが格納されているディレクトリ
-    :param output_file: 出力ファイル
+    日本語SVMの出力について、精度を計算する
+    :param experiment_dir: 実験ディレクトリ
     :return: なし
     """
-    input_files = []
-    for i in range(1, 6):
-        input_files.append(experiment_dir + "cross_validation" + str(i) + ".txt")
+    # パラメータの表示
+    print("合議対象:")
+    print("\t- 日本語文\n\t- 2段階評価\n\t- SVM")
+    print("対象ディレクトリ:")
+    print('\t- ' + experiment_dir)
+    print("-----------------------------------")
+
+    # 計算するするファイルリストを用意
+    input_files = [experiment_dir + "cross_validation" + str(i) + ".txt" for i in range(1, 6)]
 
     # 精度を計算
-    calc_accuracy.calc_accuracy_cross_validation(input_files, experiment_dir + "accuracy_file2.txt")
-
-
-def main():
-    # プロジェクトディレクトリを取得
-    base_path = os.path.dirname(os.path.abspath(__file__)) + "/../../"
-
-    # つくばコーパスについて
-    """
-    使用モデル: {SVM}
-    ウィンドウサイズ: {3, 5, 7, 3+5, 3+7, 5+7, 3+5+7}
-    補完関数: {random}
-    合議関数: {多数決, softmax}
-    """
-    ws_list = ["window3", "window5", "window7",
-               "multi_window35", "multi_window37", "multi_window57", "multi_window357"]
-    summarize_list = ["majority", "softmax"]
-
-    for ws in ws_list:
-        for summarize in summarize_list:
-            experiment_dir = base_path + "tsukuba_corpus/svm_w2v/" + ws + \
-                             "/" + "summarized_out/" + summarize + "/"
-            print("-----------------------------------")
-            print("モデル: svm")
-            print("ウィンドウサイズ: " + ws)
-            print("補完関数: random")
-            print("合議関数: " + summarize)
-            print("-----------------------------------")
-            sys.stdout.write("精度を計算中...")
-            sys.stdout.flush()
-            calculate_accuracy(experiment_dir=experiment_dir)
-            print("完了\n")
+    calc_accuracy.calc_accuracy_cross_validation(input_files, experiment_dir + "accuracy_file.txt")
 
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser(description='SVMの合議による2値分類の精度を計算する')
+    parser.add_argument("--experiment_dir", "-d")
+    args = parser.parse_args()
+
+    main(experiment_dir=args.experiment_dir)
