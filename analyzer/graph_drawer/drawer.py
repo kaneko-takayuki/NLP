4ef187ea9dfa63aab2973cb427f062c1e8048fe5
kaneko_takayuki <g.knk.9410@gmail.com>
2017-11-05 14:57:04 +0900

    グラフを描画する実行ファイルを変更

--- analyzer/graph_drawer/drawer.py
+++ analyzer/graph_drawer/drawer.py
@@ -20,15 +20,21 @@ def plot(left, input_file, label):
     plt.plot(np.array(left), np.array(height), linewidth=1, label=label)
 
 
-def draw_graph(left, input_files, labels):
+def draw_graph(left, input_files, labels, xlabel, ylabel, title):
     """
     折れ線グラフを描画する
     :param left: 横軸の長さ
     :param input_files: 描画する精度ファイルリスト
     :param labels: どの色でグラフを表示するのか
+    :param xlabel: 横軸の説明
+    :param ylabel: 縦軸の説明
+    :param title: グラフのタイトル
     :return: なし
     """
     for input_file, label in zip(input_files, labels):
         plot(left, input_file, label)
+    plt.title(title)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
     plt.legend()
     plt.show()
--- exec/analyze/exec_draw_graph_j_ffnn_w2v.py
+++ exec/analyze/exec_draw_graph_j_ffnn_w2v.py
@@ -6,7 +6,7 @@ import add_path
 from analyzer.graph_drawer import drawer
 
 
-def main(x, input_files, labels):
+def main(x, input_files, labels, xlabel, ylabel, title):
     # プロジェクトディレクトリを取得
     base_path = os.path.dirname(os.path.abspath(__file__)) + "/../../"
 
@@ -14,13 +14,16 @@ def main(x, input_files, labels):
     left = [i for i in range(1, x+1)]
 
     # グラフを描画
-    drawer.draw_graph(left, input_files, labels)
+    drawer.draw_graph(left, input_files, labels, xlabel, ylabel, title)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='折れ線グラフを描画する')
     parser.add_argument("--x", "-x", type=int, help='横軸の長さ')
     parser.add_argument("--files", "-f", nargs='*', help='グラフに表示する精度ファイル(複数指定可能)')
     parser.add_argument("--labels", "-l", nargs='*', help='線の色')
+    parser.add_argument("--xlabel", "-xl", help='横軸の説明')
+    parser.add_argument("--ylabel", "-yl", help='縦軸の説明')
+    parser.add_argument("--title", "-t", help='グラフのタイトル')
     args = parser.parse_args()
 
-    main(args.x, args.files, args.labels)
+    main(args.x, args.files, args.labels, args.xlabel, args.ylabel, args.title)
